6e5443fa01b7769a7525fef5be21606549acc745
iagodizlla <iagoschlemper@gmail.com>
2025-10-03 16:44:17 -0300

    Adicionado local-storage

--- src/app/util/pokemons-favoritos.ts
+++ src/app/util/pokemons-favoritos.ts
@@ -1,14 +1,23 @@
 import { Pokemon } from '../models/pokemon';
 
-export const pokemonsFavoritos: Pokemon[] = [];
+export const pokemonsFavoritos: Pokemon[] = obterPokemonsFavoritos();
 
-export function alternarStatusPokemon(pokemon: Pokemon) {
+export function alternarStatusPokemon(pokemon: Pokemon): void {
   if (pokemon.favorito) {
     pokemon.favorito = false;
-    const index = pokemonsFavoritos.findIndex((x) => x.id == pokemon.id);
+    const index = pokemonsFavoritos.findIndex((x) => x.id === pokemon.id);
     if (index > -1) pokemonsFavoritos.splice(index, 1);
   } else {
     pokemon.favorito = true;
     pokemonsFavoritos.push(pokemon);
   }
+  localStorage.setItem('pokedex:favoritos', JSON.stringify(pokemonsFavoritos));
+}
+
+function obterPokemonsFavoritos(): Pokemon[] {
+  var itensArmazenados = localStorage.getItem('pokedex:favoritos');
+
+  if (!itensArmazenados) return [];
+
+  return JSON.parse(itensArmazenados) as Pokemon[];
 }
