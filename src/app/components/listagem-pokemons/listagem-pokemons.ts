58fadc06ccb7384e4373c606c5e8c7356b0bcd33
iagodizlla <iagoschlemper@gmail.com>
2025-10-06 18:58:53 -0300

    Implementado PokeApiService

--- src/app/components/listagem-pokemons/listagem-pokemons.ts
+++ src/app/components/listagem-pokemons/listagem-pokemons.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Pokemon } from '../../models/pokemon';
-import { HttpClient } from '@angular/common/http';
-import { NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { converterParaTitleCase } from '../../util/converter-parap-title-case';
 import { CardPokemon } from '../card-pokemon/card-pokemon';
 import { alternarStatusPokemon, pokemonsFavoritos } from '../../util/pokemons-favoritos';
-import { PokeApiDetailsResponse, PokeApiResponse } from '../../models/poke-api';
+import { HttpClient } from '@angular/common/http';
+import { PokeApiService } from '../../services/poke-api-service';
 
 @Component({
   selector: 'app-listagem-pokemons',
@@ -20,33 +18,12 @@ export class ListagemPokemons {
   public pokemonsFavoritos = pokemonsFavoritos;
   public alternarStatusPokemon = alternarStatusPokemon;
 
-  private readonly url: string = 'https://pokeapi.co/api/v2/pokemon/';
+  private readonly pokeApiService = inject(PokeApiService);
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
 
-    this.http.get<PokeApiResponse>(this.url).subscribe((obj) => {
-      const arrayResultados: { name: string; url: string }[] = obj.results;
-
-      for (const resultado of arrayResultados) {
-        this.http.get<PokeApiDetailsResponse>(resultado.url).subscribe((objDetalhes) => {
-          const pokemon = this.mapearPokemon(objDetalhes);
-
-          this.pokemons.push(pokemon);
-        });
-      }
-    });
-  }
-
-  private mapearPokemon(obj: PokeApiDetailsResponse): Pokemon {
-
-    return {
-      id: obj.id,
-      nome: converterParaTitleCase(obj.name),
-      urlSprite: obj.sprites.front_default,
-      tipos: obj.types.map((x) => converterParaTitleCase(x.type.name)),
-      favorito: pokemonsFavoritos.some((x) => x.id == obj.id),
-    };
+     this.pokemons = this.pokeApiService.selecionarPokemons();
   }
 }
